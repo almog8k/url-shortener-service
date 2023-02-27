38a45d5e76a249daad38ca0d2e5716523cfe1ec2
almog8k <almog8k@gmail.com>
2023-02-27 10:04:30 +0200

    remove db logs

--- data-access/url-repository.ts
+++ data-access/url-repository.ts
@@ -1,7 +1,6 @@
 import { sql } from "@databases/sqlite";
 import getDbConnection from "./db-connection";
 import { randomUUID } from "crypto";
-import { SharedLogContext } from "../utils/shared-logger-context";
 
 type UrlRecord = {
   id: string;
@@ -9,16 +8,9 @@ type UrlRecord = {
   urlShortId: string;
 };
 
-const SHARED_LOG_CONTEXT: SharedLogContext = {
-  dirname: __dirname,
-  filename: __filename,
-};
-
 export async function addShortUrl(
   urlDetails: Omit<UrlRecord, "id">
 ): Promise<void> {
-  SHARED_LOG_CONTEXT.functionName = addShortUrl.name;
-  SHARED_LOG_CONTEXT.metadata = urlDetails;
   await getDbConnection().query(
     sql`INSERT INTO url_details (id, original_url, url_short_id)
             VALUES(${randomUUID()}, ${urlDetails.originalUrl}, ${
@@ -30,8 +22,6 @@ export async function addShortUrl(
 export async function getUrlByShortID(
   shortId: string
 ): Promise<string | undefined> {
-  SHARED_LOG_CONTEXT.functionName = getUrlByShortID.name;
-  SHARED_LOG_CONTEXT.metadata = { shortId };
   const results = await getDbConnection().query(sql`
         SELECT original_url FROM url_details WHERE url_short_id=${shortId};
       `);
@@ -43,14 +33,10 @@ export async function getUrlByShortID(
 }
 
 export async function isUrlShortIdExist(shortId: string): Promise<boolean> {
-  SHARED_LOG_CONTEXT.functionName = isUrlShortIdExist.name;
-  SHARED_LOG_CONTEXT.metadata = { shortId };
   return (await getUrlByShortID(shortId)) !== undefined;
 }
 
 export async function isUrlExist(urlAddress: string): Promise<boolean> {
-  SHARED_LOG_CONTEXT.functionName = isUrlExist.name;
-  SHARED_LOG_CONTEXT.metadata = { urlAddress };
   const results = await getDbConnection().query(sql`
         SELECT original_url FROM url_details WHERE original_url=${urlAddress};
       `);
