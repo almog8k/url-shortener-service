99491d3cb5364cacee4865c6ebd6b15401a26a4d
almog8k <almog8k@gmail.com>
2023-03-12 09:42:01 +0200

    adding start:prod , changing main src

--- src/start.ts
+++ src/start.ts
@@ -1,16 +1,8 @@
 import { logger } from "./utils/logger/logger-wrapper";
 import { startWebServer } from "./server";
-import util from "util";
 import { AppError, errorHandler } from "./utils/errors/error-handling";
-import { SharedLogContext } from "./utils/logger/definition";
-
-const SHARED_LOG_CONTEXT: SharedLogContext = {
-  dirname: __dirname,
-  filename: __filename,
-};
 
 async function start() {
-  SHARED_LOG_CONTEXT.functionName = start.name;
   return Promise.all([startWebServer()]);
 }
 
